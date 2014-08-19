43b58245c260b921e09a710045aeea587c5ec495
jiang echo <jiangyingji1@163.com>
2014-08-19 11:08:49 +0800

    original version

--- src/com/echo/kiss/MainActivity.java
+++ src/com/echo/kiss/MainActivity.java
@@ -2,11 +2,9 @@ package com.echo.kiss;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayout;
 
 public class MainActivity extends Activity{
 
-	GridLayout gridLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
